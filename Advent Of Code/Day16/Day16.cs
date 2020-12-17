aee9355fb12bbe701a9994b2518b846409a6a98a
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2020-12-17 15:58:48 -0500

    Day 16  Part 1 Solution

---
+++ Advent Of Code/Day16/Day16.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code.Day16
+{
+   public class Day16
+    {
+        static readonly string[] input = File.ReadAllLines(@"Day16\day16input.txt");
+        public static int SolvePart1()
+        {
+            var result = 0;
+            string[] ourTicket;
+            List<string[]> validTickets = new List<string[]>();
+            List<Rule> rules = new();
+            
+            try
+            {
+                var inputStage = 0;
+                foreach (var line in input)
+                {
+                    if (line.Trim() == String.Empty)
+                    {
+                        inputStage++;
+                        continue;
+                    }
+                    else
+                    {
+                        switch (inputStage)
+                        {
+                            case 0:
+                                string[] parts = line.Split(':');
+                                var range = parts[1].Split("or");
+                                var minRange = range[0].Trim().Split('-');
+                                var maxRange = range[1].Trim().Split('-');
+
+                                Rule rule = new Rule();
+
+                                rule.Name = parts[0];
+                                rule.MinValue1 = int.Parse(minRange[0]);
+                                rule.MinValue2 = int.Parse(minRange[1]);
+                                rule.MaxValue1 = int.Parse(maxRange[0]);
+                                rule.MaxValue2 = int.Parse(maxRange[1]);
+
+                                rules.Add(rule);
+
+                                break;
+                            case 1:
+                                if (line.Contains("your ticket:"))                              
+                                    continue;
+                                ourTicket = line.Split(',');
+                                break;
+                            case 2:
+                                if (line.Contains("nearby tickets:"))
+                                    continue;
+                                    string[] nearByTicketNumNums = line.Split(',');
+                                    int ruleIndex = 0;
+                                    for (int i = 0; i < nearByTicketNumNums.Length; i++)
+                                    {
+                                        var curNum = int.Parse(nearByTicketNumNums[i]);
+                                        var valid = false;
+                                        if ((rules[i].MinValue1 <= curNum && rules[i].MaxValue1 >= curNum) || (rules[i].MinValue2 <= curNum && rules[i].MaxValue2 >= curNum))
+                                        {
+                                            valid = true;
+                                            validTickets.Add(nearByTicketNumNums);
+                                            continue;
+                                        }
+
+                                        if (!valid)
+                                            result += curNum;
+
+                                        ruleIndex++;
+                                    }
+                                break;
+                            default:
+                                continue;
+                        }
+                    } 
+                }
+                foreach (var rule in rules)
+                {
+                    if (rule.Name.Contains("departure"))
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine($"ERROR { ex.Message}");
+            }
+            return result;
+        }
+
+        public static int SolvePart2()
+        {
+            return 0;
+        } 
+
+        class Rule
+        {
+            public string Name { get; set; }
+            public int MinValue1 { get; set; }
+            public int MaxValue1 { get; set; }
+            public int MinValue2 { get; set; }
+            public int MaxValue2 { get; set; }
+        }
+
+        class Ticket
+        {
+            public List<int> Nums { get; set; }
+        }
+    }
+}
--- Advent Of Code/Program.cs
+++ Advent Of Code/Program.cs
@@ -11,10 +11,18 @@ namespace Advent_Of_Code
     {
         static void Main(string[] args)
         {
-            //var part2 =  Day15.Day15.SolvePart2();
-            //Console.WriteLine($"Part 2: {part2}");
+            //var testInput = "this iS a sample Input string To count The index values";
+            //string letters = "abcdefghijklmnopqrstuvwxyz";
+            //int score = 0, index = 0;
+            //foreach (var letr in testInput.ToLower())
+            //{
+            //    index = letters.IndexOf(letr);
+            //    score += index;
+            //}
+            var part1 =  Day16.Day16.SolvePart1();
+            Console.WriteLine($"Part 1: {part1}");
 
-            BenchmarkRunner.Run<Benchmark.Day15>();
+            //BenchmarkRunner.Run<Benchmark.Day15>();
 
             Console.ReadLine();
         }
